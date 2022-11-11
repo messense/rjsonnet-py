a0e7e797135644c9bccafbc4922b06df31ebe7de
messense <messense@icloud.com>
2022-11-11 10:20:12 +0800

    Update jrsonnet

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,64 @@
-use std::any::Any;
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::path::{Path, PathBuf};
-use std::rc::Rc;
+use std::env;
+use std::path::PathBuf;
+use std::{any::Any, borrow::Cow};
 
-use jrsonnet_evaluator::error::LocError;
-use jrsonnet_evaluator::native::{NativeCallback, NativeCallbackHandler};
+use jrsonnet_evaluator::apply_tla;
 use jrsonnet_evaluator::{
-    ArrValue, EvaluationState, FileImportResolver, IStr, ImportResolver, LazyBinding, LazyVal,
-    ObjMember, ObjValue, Val,
+    error::{Error, ErrorKind::*},
+    function::{
+        builtin::{NativeCallback, NativeCallbackHandler},
+        TlaArg,
+    },
+    gc::GcHashMap,
+    manifest::{JsonFormat, ManifestFormat},
+    stack::set_stack_depth_limit,
+    tb,
+    trace::{CompactFormat, PathResolver, TraceFormat},
+    val::{ArrValue, StrValue},
+    FileImportResolver, IStr, ImportResolver, ObjValue, State, Val,
 };
-use jrsonnet_gc::{unsafe_empty_trace, Finalize, Gc, Trace};
-use jrsonnet_parser::{Param, ParamsDesc, Visibility};
+use jrsonnet_gcmodule::Trace;
+use jrsonnet_parser::{ParserSettings, Source, SourceDirectory, SourceFile, SourcePath};
 use pyo3::exceptions::{PyRuntimeError, PyTypeError};
 use pyo3::prelude::*;
 use pyo3::types::{PyBool, PyDict, PyFloat, PyList, PySequence, PyString, PyTuple};
 
+#[derive(Trace)]
 struct PythonImportResolver {
+    #[trace(skip)]
     callback: PyObject,
-    out: RefCell<HashMap<PathBuf, IStr>>,
+    out: RefCell<HashMap<SourcePath, Vec<u8>>>,
 }
 
 impl ImportResolver for PythonImportResolver {
-    fn resolve_file(
+    fn resolve_from(
         &self,
-        from: &Path,
-        path: &Path,
-    ) -> jrsonnet_evaluator::error::Result<Rc<Path>> {
-        use jrsonnet_evaluator::error::Error::*;
-
+        from: &SourcePath,
+        path: &str,
+    ) -> jrsonnet_evaluator::error::Result<SourcePath> {
+        let base = if let Some(file) = from.downcast_ref::<SourceFile>() {
+            let mut path = file.path().to_path_buf();
+            path.pop();
+            path
+        } else if let Some(dir) = from.downcast_ref::<SourceDirectory>() {
+            dir.path().to_path_buf()
+        } else if from.is_default() {
+            env::current_dir().map_err(|e| ImportIo(e.to_string()))?
+        } else {
+            let err_msg = "can't resolve this path";
+            Python::with_gil(|py| {
+                let err = PyRuntimeError::new_err(err_msg);
+                err.restore(py);
+            });
+            return Err(Error::new(ImportCallbackError(format!(
+                "import_callback error: {}",
+                err_msg
+            ))));
+        };
         let (resolved, content) =
-            Python::with_gil(|py| match self.callback.call(py, (from, path), None) {
+            Python::with_gil(|py| match self.callback.call(py, (base, path), None) {
                 Ok(obj) => obj.extract::<(String, Option<String>)>(py).map_err(|err| {
                     let err_msg = err.to_string();
                     err.restore(py);
@@ -46,29 +74,32 @@ impl ImportResolver for PythonImportResolver {
                 }
             })?;
         if let Some(content) = content {
-            let resolved = PathBuf::from(resolved);
+            let resolved = SourcePath::new(SourceFile::new(PathBuf::from(resolved)));
             let mut out = self.out.borrow_mut();
             if !out.contains_key(&resolved) {
                 out.insert(resolved.clone(), content.into());
             }
-            Ok(resolved.into())
+            Ok(resolved)
         } else {
-            Err(ImportFileNotFound(from.to_path_buf(), path.to_path_buf()).into())
+            Err(ImportFileNotFound(from.clone(), path.to_string()).into())
         }
     }
 
-    fn load_file_contents(&self, resolved: &Path) -> jrsonnet_evaluator::error::Result<IStr> {
+    fn load_file_contents(
+        &self,
+        resolved: &SourcePath,
+    ) -> jrsonnet_evaluator::error::Result<Vec<u8>> {
         Ok(self.out.borrow().get(resolved).unwrap().clone())
     }
 
-    unsafe fn as_any(&self) -> &dyn Any {
-        panic!("this resolver can't be used as any")
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
 fn pyobject_to_val(py: Python, obj: PyObject) -> PyResult<Val> {
     return if let Ok(s) = obj.downcast::<PyString>(py) {
-        s.to_str().map(|s| Val::Str(s.into()))
+        s.to_str().map(|s| Val::Str(StrValue::Flat(s.into())))
     } else if let Ok(b) = obj.downcast::<PyBool>(py) {
         Ok(Val::Bool(b.is_true()))
     } else if let Ok(f) = obj.downcast::<PyFloat>(py) {
@@ -79,26 +110,18 @@ fn pyobject_to_val(py: Python, obj: PyObject) -> PyResult<Val> {
         Ok(Val::Null)
     } else if let Ok(seq) = obj.downcast::<PySequence>(py) {
         let len = seq.len()?;
-        let mut arr = Vec::with_capacity(len as usize);
+        let mut arr = Vec::with_capacity(len);
         for i in 0..len {
             let item = seq.get_item(i)?;
             arr.push(pyobject_to_val(py, item.into_py(py))?);
         }
-        Ok(Val::Arr(ArrValue::Eager(Gc::new(arr))))
+        Ok(Val::Arr(ArrValue::eager(arr)))
     } else if let Ok(d) = obj.downcast::<PyDict>(py) {
         let mut map = ObjValue::new_empty();
         for (k, v) in d {
             let k = k.extract::<String>()?;
             let v = pyobject_to_val(py, v.into_py(py))?;
-            map = map.extend_with_field(
-                k.into(),
-                ObjMember {
-                    add: false,
-                    visibility: Visibility::Normal,
-                    invoke: LazyBinding::Bound(LazyVal::new_resolved(v)),
-                    location: None,
-                },
-            );
+            map.extend_field(k.into()).value(v);
         }
         Ok(Val::Obj(map))
     } else {
@@ -112,7 +135,7 @@ fn val_to_pyobject(py: Python, val: &Val) -> PyObject {
     match val {
         Val::Bool(b) => b.into_py(py),
         Val::Null => py.None(),
-        Val::Str(s) => s.into_py(py),
+        Val::Str(s) => s.clone().into_flat().into_py(py),
         Val::Num(n) => n.into_py(py),
         Val::Arr(a) => {
             let arr = PyList::empty(py);
@@ -134,19 +157,16 @@ fn val_to_pyobject(py: Python, val: &Val) -> PyObject {
     }
 }
 
+#[derive(Trace)]
 struct JsonnetNativeCallbackHandler {
+    #[trace(skip)]
     name: String,
+    #[trace(skip)]
     func: PyObject,
 }
 
-impl Finalize for JsonnetNativeCallbackHandler {}
-
-unsafe impl Trace for JsonnetNativeCallbackHandler {
-    unsafe_empty_trace!();
-}
-
 impl NativeCallbackHandler for JsonnetNativeCallbackHandler {
-    fn call(&self, _from: Option<Rc<Path>>, args: &[Val]) -> Result<Val, LocError> {
+    fn call(&self, args: &[Val]) -> Result<Val, Error> {
         Python::with_gil(|py| {
             let args: Vec<_> = args.iter().map(|v| val_to_pyobject(py, v)).collect();
             let err = match self.func.call(py, PyTuple::new(py, args), None) {
@@ -158,93 +178,142 @@ impl NativeCallbackHandler for JsonnetNativeCallbackHandler {
             };
             let err_msg = err.to_string();
             err.restore(py);
-            Err(LocError::new(
-                jrsonnet_evaluator::error::Error::RuntimeError(
-                    format!("error invoking native extension {}: {}", self.name, err_msg).into(),
-                ),
-            ))
+            Err(Error::new(RuntimeError(
+                format!("error invoking native extension {}: {}", self.name, err_msg).into(),
+            )))
         })
     }
 }
 
-#[allow(clippy::too_many_arguments)]
-#[inline]
-fn create_evaluation_state(
-    py: Python,
-    jpathdir: Option<Vec<PathBuf>>,
-    max_stack: usize,
-    ext_vars: HashMap<String, String>,
-    ext_codes: HashMap<String, String>,
-    tla_vars: HashMap<String, String>,
-    tla_codes: HashMap<String, String>,
-    max_trace: usize,
-    import_callback: Option<PyObject>,
-    native_callbacks: HashMap<String, (PyObject, PyObject)>,
-) -> PyResult<EvaluationState> {
-    let vm = EvaluationState::default();
-    vm.set_max_stack(max_stack);
-    vm.set_max_trace(max_trace);
-    for (k, v) in ext_vars.into_iter() {
-        vm.add_ext_str(k.into(), v.into());
-    }
-    for (k, v) in ext_codes.into_iter() {
-        vm.add_ext_code(k.into(), v.into())
-            .map_err(|e| PyRuntimeError::new_err(vm.stringify_err(&e)))?;
-    }
-    for (k, v) in tla_vars.into_iter() {
-        vm.add_tla_str(k.into(), v.into());
-    }
-    for (k, v) in tla_codes.into_iter() {
-        vm.add_tla_code(k.into(), v.into())
-            .map_err(|e| PyRuntimeError::new_err(vm.stringify_err(&e)))?;
-    }
-
-    if let Some(import_callback) = import_callback {
-        if !import_callback.as_ref(py).is_callable() {
-            return Err(PyTypeError::new_err("import_callback must be callable"));
-        }
-        let import_resolver = PythonImportResolver {
-            callback: import_callback,
-            out: RefCell::new(HashMap::new()),
-        };
-        vm.set_import_resolver(Box::new(import_resolver));
-    } else if let Some(jpathdir) = jpathdir {
-        let import_resolver = FileImportResolver {
-            library_paths: jpathdir,
-        };
-        vm.set_import_resolver(Box::new(import_resolver));
-    }
-
-    for (name, (args, func)) in native_callbacks.into_iter() {
-        let args = args.downcast::<PyTuple>(py)?;
-        let mut params = Vec::with_capacity(args.len());
-        for arg in args {
-            let param = arg.extract::<&str>()?;
-            params.push(Param(param.into(), None));
-        }
-        let params = ParamsDesc(Rc::new(params));
-        vm.add_native(
-            name.clone().into(),
-            Gc::new(NativeCallback::new(
-                params,
-                Box::new(JsonnetNativeCallbackHandler { name, func }),
-            )),
-        );
-    }
-    Ok(vm)
+struct VirtualMachine {
+    state: State,
+    manifest_format: Box<dyn ManifestFormat>,
+    trace_format: Box<dyn TraceFormat>,
+    tla_args: GcHashMap<IStr, TlaArg>,
 }
 
-fn loc_error_to_pyerr(py: Python, vm: &EvaluationState, loc_err: &LocError) -> PyErr {
-    let cause = if PyErr::occurred(py) {
-        Some(PyErr::fetch(py))
-    } else {
-        None
-    };
-    let py_err = PyRuntimeError::new_err(vm.stringify_err(loc_err));
-    if cause.is_some() {
-        py_err.set_cause(py, cause);
+impl VirtualMachine {
+    #[allow(clippy::too_many_arguments)]
+    #[inline]
+    fn new(
+        py: Python,
+        jpathdir: Option<Vec<PathBuf>>,
+        max_stack: usize,
+        ext_vars: HashMap<String, String>,
+        ext_codes: HashMap<String, String>,
+        tla_vars: HashMap<String, String>,
+        tla_codes: HashMap<String, String>,
+        max_trace: usize,
+        import_callback: Option<PyObject>,
+        native_callbacks: HashMap<String, (PyObject, PyObject)>,
+    ) -> PyResult<Self> {
+        let state = State::default();
+        set_stack_depth_limit(max_stack);
+
+        state.settings_mut().import_resolver = tb!(FileImportResolver::default());
+
+        let trace_format = CompactFormat {
+            max_trace,
+            ..Default::default()
+        };
+
+        let context_initializer = jrsonnet_stdlib::ContextInitializer::new(
+            state.clone(),
+            PathResolver::new_cwd_fallback(),
+        );
+
+        for (k, v) in ext_vars.into_iter() {
+            context_initializer.add_ext_str(k.into(), v.into());
+        }
+        for (k, v) in ext_codes.into_iter() {
+            context_initializer
+                .add_ext_code(k.as_str(), v.as_str())
+                .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+        }
+
+        let mut tla_args = GcHashMap::new();
+
+        for (k, v) in tla_vars.into_iter() {
+            tla_args.insert(k.into(), TlaArg::String(v.into()));
+        }
+        for (k, v) in tla_codes.into_iter() {
+            let name: IStr = k.into();
+            let code: IStr = v.clone().into();
+            let code = jrsonnet_parser::parse(
+                &v,
+                &ParserSettings {
+                    source: Source::new_virtual(format!("<top-level-arg:{name}>").into(), code),
+                },
+            )
+            .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+            tla_args.insert(name, TlaArg::Code(code));
+        }
+
+        if let Some(import_callback) = import_callback {
+            if !import_callback.as_ref(py).is_callable() {
+                return Err(PyTypeError::new_err("import_callback must be callable"));
+            }
+            let import_resolver = PythonImportResolver {
+                callback: import_callback,
+                out: RefCell::new(HashMap::new()),
+            };
+            state.set_import_resolver(import_resolver);
+        } else if let Some(jpathdir) = jpathdir {
+            let import_resolver = FileImportResolver::new(jpathdir);
+            state.set_import_resolver(import_resolver);
+        }
+
+        for (name, (args, func)) in native_callbacks.into_iter() {
+            let args = args.downcast::<PyTuple>(py)?;
+            let mut params = Vec::with_capacity(args.len());
+            for arg in args {
+                let param = arg.extract::<String>()?;
+                params.push(Cow::Owned(param));
+            }
+            context_initializer.add_native(
+                name.clone().into(),
+                #[allow(deprecated)]
+                NativeCallback::new(params, JsonnetNativeCallbackHandler { name, func }),
+            );
+        }
+
+        state.settings_mut().context_initializer = tb!(context_initializer);
+        Ok(Self {
+            state,
+            manifest_format: Box::new(JsonFormat::default()),
+            trace_format: Box::new(trace_format),
+            tla_args,
+        })
     }
-    py_err
+
+    fn evaluate_file(&self, filename: &str) -> Result<String, Error> {
+        self.state
+            .import_from(&SourcePath::new(SourceDirectory::new(".".into())), filename)
+            .and_then(|val| apply_tla(self.state.clone(), &self.tla_args, val))
+            .and_then(|val| val.manifest(&self.manifest_format))
+    }
+
+    fn evaluate_snippet(&self, filename: &str, snippet: &str) -> Result<String, Error> {
+        self.state
+            .evaluate_snippet(filename, snippet)
+            .and_then(|val| apply_tla(self.state.clone(), &self.tla_args, val))
+            .and_then(|val| val.manifest(&self.manifest_format))
+    }
+
+    fn error_to_pyerr(&self, py: Python, err: &Error) -> PyErr {
+        let cause = if PyErr::occurred(py) {
+            Some(PyErr::fetch(py))
+        } else {
+            None
+        };
+        let mut err_msg = String::new();
+        self.trace_format.write_trace(&mut err_msg, err).unwrap();
+        let py_err = PyRuntimeError::new_err(err_msg);
+        if cause.is_some() {
+            py_err.set_cause(py, cause);
+        }
+        py_err
+    }
 }
 
 #[derive(FromPyObject)]
@@ -293,8 +362,7 @@ fn evaluate_file(
     import_callback: Option<PyObject>,
     native_callbacks: HashMap<String, (PyObject, PyObject)>,
 ) -> PyResult<String> {
-    let path = PathBuf::from(filename);
-    let vm = create_evaluation_state(
+    let vm = VirtualMachine::new(
         py,
         jpathdir.map(|x| x.into_vec()),
         max_stack,
@@ -308,12 +376,9 @@ fn evaluate_file(
     )?;
 
     let result = vm
-        .with_stdlib()
-        .evaluate_file_raw_nocwd(&path)
-        .and_then(|v| vm.with_tla(v))
-        .and_then(|v| vm.manifest(v))
-        .map_err(|e| loc_error_to_pyerr(py, &vm, &e))?;
-    Ok(result.to_string())
+        .evaluate_file(filename)
+        .map_err(|e| vm.error_to_pyerr(py, &e))?;
+    Ok(result)
 }
 
 /// Evaluate jsonnet code snippet
@@ -349,8 +414,7 @@ fn evaluate_snippet(
     import_callback: Option<PyObject>,
     native_callbacks: HashMap<String, (PyObject, PyObject)>,
 ) -> PyResult<String> {
-    let path = PathBuf::from(filename);
-    let vm = create_evaluation_state(
+    let vm = VirtualMachine::new(
         py,
         jpathdir.map(|x| x.into_vec()),
         max_stack,
@@ -364,12 +428,9 @@ fn evaluate_snippet(
     )?;
 
     let result = vm
-        .with_stdlib()
-        .evaluate_snippet_raw(path.into(), src.into())
-        .and_then(|v| vm.with_tla(v))
-        .and_then(|v| vm.manifest(v))
-        .map_err(|e| loc_error_to_pyerr(py, &vm, &e))?;
-    Ok(result.to_string())
+        .evaluate_snippet(filename, src)
+        .map_err(|e| vm.error_to_pyerr(py, &e))?;
+    Ok(result)
 }
 
 /// Python bindings to Rust jrsonnet crate
